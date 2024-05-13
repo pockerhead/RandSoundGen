80cd6609ecfed562ad255e6f874ae8cfc54314ca
Artem Balashov <pockerhead@ya.ru>
2024-05-13 20:32:35 +0300

    Initial Commit

---
+++ RandSoundGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandSoundGen
+//
+//  Created by Артём Балашов on 13.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
